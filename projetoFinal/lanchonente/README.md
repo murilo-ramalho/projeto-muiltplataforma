c1155652ce5e2bc8b642deb0ff34630c13f8240f
Murilo Ramalho <murilo.ramalho@proton.me>
2024-11-26 21:35:37 -0300

    projeto inicial do crud

---
+++ projetoFinal/lanchonente/README.md
@@ -0,0 +1,8 @@
+# lanchonente
+
+crud flutter com tema de lanchonete
+
+## grupo
+
+- Murilo Ramalho da Mata
+- Camila Gomes da Silva Casa
---
+++ projetoFinal/lanchonente/android/app/src/main/kotlin/com/example/lanchonente/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lanchonente
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

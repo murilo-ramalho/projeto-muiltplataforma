fe12e4bede8ad311669a412f071ee54115c17dc3
Murilo Ramalho <murilo.ramalho@proton.me>
2024-11-28 19:44:19 -0300

    removido para novo repo

--- projetoFinal/lanchonente/README.md
+++
@@ -1,8 +0,0 @@
-# lanchonente
-
-crud flutter com tema de lanchonete
-
-## grupo
-
-- Murilo Ramalho da Mata
-- Camila Gomes da Silva Casa
--- projetoFinal/lanchonente/android/app/src/main/kotlin/com/example/lanchonente/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lanchonente
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
